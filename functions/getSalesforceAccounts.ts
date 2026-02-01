d9d3718e5bc5b4f56aa337e6085e702d075c4f38
Katy Parrish <katy.parrish@adtrak.co.uk>
2026-02-01 17:50:16 +0000

    File changes

--- functions/getSalesforceAccounts.ts
+++ functions/getSalesforceAccounts.ts
@@ -13,7 +13,7 @@ Deno.serve(async (req) => {
     const dataSources = await base44.asServiceRole.entities.DataSource.filter({
       organization_id: user.organization_id,
       platform_type: 'salesforce'
-    });
+    }, '-updated_date', 1);
 
     let dataSource = dataSources[0];
     let whereClause = '';
